2bb85822e7bfdd68f25e92d3d71a5b3ce8cc7661
Student <Student@205-46>
2023-11-21 15:21:31 +0800

    加入專案檔案。

---
+++ NewsApp/Services/ApiService.cs
@@ -0,0 +1,20 @@
+﻿using NewsApp.models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Services
+{
+    public class ApiService
+    {
+        public async Task<Root> GetNews(string categoryName)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync($"https://gnews.io/api/v4/top-headlines?category={categoryName.ToLower()}&apikey=9364d2f3a7aab238348584e3ee7d911a");
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
